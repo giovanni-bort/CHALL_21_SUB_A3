1decd7f3bf1612fa871c8f3d9dc733b83b09bf60
giovanni-bort <63361927+giovanni-bort@users.noreply.github.com>
2021-03-29 23:11:14 +0200

    Add files via upload

---
+++ loop_cre_IMG.m
@@ -0,0 +1,9 @@
+%function loop_cre_IMG
+
+% my_train_model(dir_CINC1,'NEW_IMG1',1);
+% 
+% my_train_model(dir_CINC2,'NEW_IMG1',1);
+% 
+% my_train_model(dir_CINC6,'NEW_IMG1',1);
+
+my_train_model(dir_CINC5,'NEW_IMG1',8091);
